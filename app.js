8ea570651e6e0805ed553aaaf8abd8ec94d1622f
DAVIDSON LUCAS SALES DE OLIVEIRA <106599546+DavidsonLucas@users.noreply.github.com>
2023-02-04 02:05:17 -0300

    algunhas mudaças

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const button = document.getElementById('convert-button')
 const select = document.getElementById('currency-select')
 
 
-const dolar = 5.2
-const euro = 5.9
+const dolar = 5.11
+const euro = 5.55
 const bitcoin = 0.0000084
 
 const convertValues = () => {
@@ -41,18 +41,23 @@ const convertValues = () => {
 chagecurrency = () => {
     const trocarNime = document.getElementById('currency-name0')
     const trocarImg = document.getElementById('img-currency')
+    const trocarBack= document.querySelector('section')
 
     if (select.value === '€ Euro') {
         trocarNime.innerHTML = 'Euro'
         trocarImg.src = 'assets/euro.svg'
+        trocarBack.style.backgroundImage = `url(${'assets/ereopeia.gif'})`;
     } else {
         trocarNime.innerHTML = 'Dólar Americano'
         trocarImg.src = 'assets/estados-unidos (1) 1.svg'
+        trocarBack.style.backgroundImage = `url(${'assets/badeira-do-estadosunidos.gif'})`;
     }
 
     if(select.value === 'Bitcoin'){
         trocarNime.innerHTML = 'Bitcoin'
         trocarImg.src = 'assets/biticon.png'
+        trocarBack.style.backgroundImage = `url(${'assets/biticon-bandeira.gif'})`;
+
     }
 
     convertValues();
